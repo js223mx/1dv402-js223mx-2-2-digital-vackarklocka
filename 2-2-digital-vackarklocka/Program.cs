1056683da39e720374902a881bcb70c1d552bde4
js223mx <js223mx@student.lnu.se>
2014-10-26 23:30:39 +0100

    allmost done with program.cs

--- 2-2-digital-vackarklocka/Program.cs
+++ 2-2-digital-vackarklocka/Program.cs
@@ -11,23 +11,106 @@ namespace _2_2_digital_vackarklocka
         static void Main(string[] args) 
         {
             //test 1
-            ViewTestHeader("");
+            ViewTestHeader("Test 1.\n Test av standardkonstruktorn");
+            AlarmClock myTestClock1 = new AlarmClock();
+            Console.WriteLine(myTestClock1.ToString);
 
             //test 2
-            ViewTestHeader("");
+            ViewTestHeader("Test 2.\n Test av konstruktorn med två parametrar, <9, 42>");
+            AlarmClock myTestClock2 = new AlarmClock(9, 42);
+            Console.WriteLine(myTestClock2.ToString());
 
             //test 3
-            ViewTestHeader("");
+            ViewTestHeader("Test 3.\n Test av konstruktorn med fyra parametrar, <13, 24, 7, 35>");
+            AlarmClock myTestClock3 = new AlarmClock(13, 24, 7, 35);
+            Console.WriteLine(myTestClock3.ToString());
 
             //test 4
-            ViewTestHeader("");
+            ViewTestHeader("Test 4. \n Ställer befintlig AlarmClock-object till 23:58 och låter den gå 13 minuter.");
+            myTestClock3.Hour = 23;
+            myTestClock3.Minute = 58;
+            Run(myTestClock3, 13);
 
             //test 5
-            ViewTestHeader("");
+            ViewTestHeader("Test 5. \n Ställer befintlig AlarmClock-object till tiden 6:12 och alarmtiden till 6:15 och låter den gå 6 minuter");
+            myTestClock3.Hour = 6;
+            myTestClock3.Minutes = 12;
+            myTestClock3.AlarmHour = 6;
+            myTestClock3.AlarmMinute = 15;
+            Run(myTestClock3, 6);
 
             //test 6
-            ViewTestHeader("");
+            ViewTestHeader("Test 6.\n Testar egenskaperna så att undantag kastas då tid och alarmtid tilldelas felaktiga värden");
 
+            try 
+            {
+                myTestClock3.Hour = 24;
+            }
+            catch (ArgumentException)
+            {
+                ViewErrorMessage("Timmen är inte i intervallet mellan 0-23.");
+            }
+            try 
+            {
+                myTestClock3.Minutes = 60;
+            }
+            catch (ArgumentException) 
+            {
+                ViewErrorMessage("Minuterna är inte i intervallet mellan 0-59.");
+            }
+            try
+            {
+                myTestClock3.AlarmHour = 24;
+            }
+            catch (ArgumentException) 
+            {
+                ViewErrorMessage("Alarmtimmen är inte i intervallet mellan 0-23.");
+            }
+            try 
+            {
+                myTestClock3.AlarmMinute = 60;
+            }
+            catch (ArgumentException) 
+            {
+                ViewErrorMessage("Alarmminuten är inte i intervallet mellan 0-59.");
+            }
+
+
+            //test7
+            ViewTestHeader("Test 7.\n Testar konstruktorerna så att undantag kastas då tid och alarmtid tilldelas felaktiga värden");
+
+            try 
+            {
+                AlarmClock myTestClock4 = new AlarmClock(24, 0);
+            }
+            catch (ArgumentException) 
+            {
+                ViewErrorMessage("Timmen är inte i intervallet 0-23.");
+            }
+            try 
+            {
+                AlarmClock myTestClock5 = new AlarmClock(0, 60);
+            }
+            catch (ArgumentException) 
+            {
+                ViewErrorMessage("Minuten är inte i intervallet 0-59.");
+            }
+            try 
+            {
+                AlarmClock myTestClock6 = new AlarmClock(0, 0, 24, 0);
+            }
+            catch (ArgumentException) 
+            {
+                ViewErrorMessage("Alarmtimmen är inte i intervallen 0-23.");
+            }
+            try 
+            {
+                AlarmClock myTestClock7 = new AlarmClock(0, 0, 0, 60);
+            }
+            catch (ArgumentException) 
+            {
+                ViewErrorMessage("Alarmminuten är inte i intervallet 0-59.");
+            }
         }
         private static void Run(AlarmClock ac, int minutes)
         {
@@ -58,10 +141,16 @@ namespace _2_2_digital_vackarklocka
         private static void ViewErrorMessage(string message)
         {
             //felmeddelande skrivs ut
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
         private static void ViewTestHeader(string header) 
         {
-            
+            Console.Write("================================================================");
+            Console.WriteLine(header);
+            Console.WriteLine();
         }
     }
 }
